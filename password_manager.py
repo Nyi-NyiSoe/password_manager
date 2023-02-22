629aa9bf4d1c779846a91b68d412f38ca0fcd1f9
Jack <jackemrick2k@gmail.com>
2023-02-22 21:38:59 +0700

    added methods and class

--- password_manager.py
+++ password_manager.py
@@ -1,12 +1,39 @@
 from tkinter import *
 import random
 
-lower = "abcdefghijklmnopqrstuvwxyz"
-upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-numbers = "1234567890"
-sp_char = "@#$&_-()=%*:/!?+."
+def frame(root,side):
+    w = Frame(root)
+    w.pack(side=side,expand=YES,fill=BOTH)
+    return w
 
-str = lower + upper + numbers + sp_char
-length  = int(input("enter the length of your password! "))
-password = "".join(random.sample(str,8))
-print( password)
+def button(root,side,text,command=None):
+    w = Button(root,text=text,command=command)
+    w.pack(side=side,expand=YES,fill=BOTH)
+
+    return w
+
+def create():
+    lower = "abcdefghijklmnopqrstuvwxyz"
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    numbers = "1234567890"
+    sp_char = "@#$&_-()=%*:/!?+."
+
+    str = lower + upper + numbers + sp_char
+    length  = int(input("enter the length of your password! "))
+    password = "".join(random.sample(str,8))
+    return password
+
+class psw_mnger(Frame):
+    def __init__(self):
+        Frame.__init__(self)
+        self.pack(expand=YES,fill=BOTH)
+        self.master.title('Password Manager')
+
+        display = StringVar()
+        Entry(self,relief=SUNKEN,textvariable=display).grid(row=0,column=0)
+
+
+
+
+if __name__ =='__main__':
+    psw_mnger().mainloop()
