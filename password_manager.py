321935be60fe8da811e85b96b8225a15285bded2
Jack <jackemrick2k@gmail.com>
2023-02-22 23:25:58 +0700

    added buttons and adjusted positions

--- password_manager.py
+++ password_manager.py
@@ -1,17 +1,9 @@
 from tkinter import *
 import random
 
-def frame(root,side):
-    w = Frame(root)
-    w.pack(side=side,expand=YES,fill=BOTH)
-    return w
 
-def button(root,side,text,command=None):
-    w = Button(root,text=text,command=command)
-    w.pack(side=side,expand=YES,fill=BOTH)
 
-    return w
-
+#create_password
 def create():
     lower = "abcdefghijklmnopqrstuvwxyz"
     upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -24,16 +16,49 @@ def create():
     return password
 
 class psw_mnger(Frame):
-    def __init__(self):
-        Frame.__init__(self)
+    def __init__(self,master):
+        Frame.__init__(self,master)
         self.pack(expand=YES,fill=BOTH)
         self.master.title('Password Manager')
-
-        display = StringVar()
-        Entry(self,relief=SUNKEN,textvariable=display).grid(row=0,column=0)
+        master.geometry("800x200")
 
 
+        #website_label
+        Label(self,relief=SUNKEN,font='20',text='Website',width=10,height=1).grid(row=0,column=0,padx=10,pady=10)
 
+        #website_name_entry
+        website_name = StringVar()
+        Entry(self,relief=SUNKEN,font='20',textvariable=website_name,width=40).grid(row=0,column=2)
+
+        #email_label
+        Label(self,relief=SUNKEN,font='20',text='Email',width=10).grid(row=1,column=0,padx=10,pady=10)
+
+        #email_entry
+        email = StringVar()
+        Entry(self,relief=SUNKEN,font='20',textvariable=email,width=40).grid(row=1,column=2)
+
+        #password_label
+        Label(self,relief=SUNKEN,font='20',text='Password',width=10).grid(row=2,column=0,padx=10,pady=10)
+
+        #password_entry
+        password = StringVar()
+        Entry(self,relief=SUNKEN,font='20',textvariable=password,width=40).grid(row=2,column=2)
+
+        #Load_button
+        Button(master,text='Load',font='20',command=create,width=10,height=1).pack(side=LEFT,padx=10)
+
+        #create_button
+        Button(master,text='Create',font='20',command=create,width=10,height=1).pack(side=LEFT,padx=75)
+
+        #save_button
+        Button(master,text='Save',font='20',command=create,width=10,height=1).pack(side=LEFT,padx=10)
+       
+        
+        
+        
 
 if __name__ =='__main__':
-    psw_mnger().mainloop()
+    root = Tk()
+    view = psw_mnger(root)
+    view.pack()
+    view.mainloop()
