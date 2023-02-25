f1ffbb577de05dc8fbb098c8bb55af0b31f62c37
Jack <jackemrick2k@gmail.com>
2023-02-25 16:37:20 +0700

    added save method to write in excel file

--- password_manager.py
+++ password_manager.py
@@ -1,6 +1,8 @@
 from tkinter import *
 import random
-
+import pandas as pd 
+import openpyxl
+import os
 
 
 #create_password
@@ -15,6 +17,27 @@ def create():
     password = "".join(random.sample(str,8))
     return password
 
+#save_data_in_excel
+def save(plat,email,psw):
+    data = {"Platform":[plat],"Email":[email],"Password":[psw]}
+    df = pd.DataFrame(data,columns=["Platform","Email","Password"])
+    
+    if not os.path.isfile("password.xlsx"):
+        
+        df.to_excel('password.xlsx',index=False,header=True)
+    else:
+        existing_df = pd.read_excel("password.xlsx")
+        new_df = pd.concat([existing_df,df],ignore_index=True)
+        new_df.to_excel("password.xlsx",index=False)
+
+        
+   
+    
+    print("successful",os.getcwd())
+
+
+
+
 class psw_mnger(Frame):
     def __init__(self,master):
         Frame.__init__(self,master)
@@ -23,37 +46,40 @@ class psw_mnger(Frame):
         master.geometry("850x200")
 
 
-        #website_label
-        Label(self,relief=SUNKEN,font='20',text='Website',width=10,height=1).grid(row=0,column=0,padx=10,pady=10)
+        #platform_label
+        Label(self,relief=SUNKEN,font='20',text='Platform',width=10,height=1).grid(row=0,column=0,padx=10,pady=10)
 
-        #website_name_entry
-        website_name = StringVar()
-        Entry(self,relief=SUNKEN,font='20',textvariable=website_name,width=40).grid(row=0,column=2)
+        #platform_name_entry
+        self.platform_name = StringVar()
+        Entry(self,justify=CENTER,relief=SUNKEN,font='20',textvariable=self.platform_name,width=40).grid(row=0,column=2)
 
         #email_label
         Label(self,relief=SUNKEN,font='20',text='Email',width=10).grid(row=1,column=0,padx=10,pady=10)
 
         #email_entry
-        email = StringVar()
-        Entry(self,relief=SUNKEN,font='20',textvariable=email,width=40).grid(row=1,column=2)
+        self.email = StringVar()
+        Entry(self,justify=CENTER,relief=SUNKEN,font='20',textvariable=self.email,width=40).grid(row=1,column=2)
 
         #password_label
         Label(self,relief=SUNKEN,font='20',text='Password',width=10).grid(row=2,column=0,padx=10,pady=10)
 
         #password_entry
-        password = StringVar()
-        Entry(self,relief=SUNKEN,font='20',textvariable=password,width=40).grid(row=2,column=2)
-
-        
+        self.password = StringVar()
+        Entry(self,justify=CENTER,relief=SUNKEN,font='20',textvariable=self.password,width=40).grid(row=2,column=2)
 
         #Load_button
-        Button(master,text='Load',font='20',command=create,width=10,height=1).pack(side=LEFT,padx=10)
+        load_btn = Button(master,text='Load',font='20',command=create,width=10,height=1)
+        load_btn.pack(side=LEFT,padx=10)
+        
 
         #create_button
-        Button(master,text='Create',font='20',command=create,width=10,height=1).pack(side=LEFT,padx=75)
+        create_btn = Button(master,text='Create',font='20',command=None,width=10,height=1)
+        create_btn.pack(side=LEFT,padx=75)
 
         #save_button
-        Button(master,text='Save',font='20',command=create,width=10,height=1).pack(side=LEFT,padx=10)
+        save_btn = Button(master,text='Save',font='20',width=10,height=1,command=lambda :save(self.platform_name.get(),self.email.get(),self.password.get()))
+        save_btn.pack(side=LEFT)
+        
 
           
 
